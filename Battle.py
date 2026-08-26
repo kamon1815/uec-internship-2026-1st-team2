9842c06f65fa44413b8fdcba5e793aa51454438f
UEC4 <pj_internship_UEC4@photron.co.jp>
2026-08-26 10:44:39 +0900

    コメントの追加

--- Battle.py
+++ Battle.py
@@ -19,14 +19,15 @@ import time
 class Battle:
 
     battle_window = "BattleWindow"
-    oneshotFlags = [True] * 6   
-    rsp_gestures = []
-    rsp_rates = []
+    rsp_gestures = [] #ジェスチャー認識の結果
+    rsp_rates = [] # 手の確率の推定結果
 
+    #初期設定
     def __init__(self,cameramode = 0, capturemode = 0):
         self.cameraMode = cameramode# 0: inficicamを使用する 1: webカメラを使用する
         self.captureMode = capturemode# 0: 2つの画像で判定(確率の増分での判断に使用)  1: 10枚の画像で判定(統計的判断に使用)
 
+        #infinicamとwebカメラをセットアップ
         try:
             self.__infinicam = InfinicamManager()
             self.__infinicam.connect(988,640,525,2.0,10) #fps , 解像度縦横, コントラスト, 明るさ
@@ -46,23 +47,32 @@ class Battle:
         self.__finalans = finalanswer(10)
 
 
+    def changeCameraConfig(self,config):
+        self.__infinicam.configurateCameraImage(config.get('contrast'),config.get('light'))#カメラの設定変更(仮)
+
+
+    #現在のカメラ設定に応じてカメラから画像を取得する -------------------------------------------------------------------
     def getCameraImage(self):
 
-        if self.cameraMode == 0:
+        if self.cameraMode == 0: # infinicamを使う場合
             if self.__infinicam != None: 
                 img,defimg = self.__infinicam.get_frame()
                 img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
             else: 
-                self.cameraMode = 1
+                self.cameraMode = 1 # infinicamが接続されていないならwebカメラを使うようにする
 
-        if self.cameraMode == 1:
+        if self.cameraMode == 1: # webカメラを使う場合
             ret,img = self.__cap.read()
+
         return img
 
 
+    #カメラ切り替えの機能 ---------------------------------------------------------------------------------------------
     def changeCamera(self,c):#c = 0でinfinicam、c = 1 でwebカメラ
         self.cameraMode = c
 
+
+    #骨格推定を実行 (旧版)  ------------------------------------------------------------------------------------------
     def computeRPS(self, maxtrial = 40,amount = 2, duration = 100 ): # 画像取得からパーセンテージの計算までを行います   maxtrialは撮影失敗時に取り直す回数、 amountは最終的に出力する枚数、durationは撮影間隔
 
         rsp_rates = []
@@ -90,18 +100,18 @@ class Battle:
 
 
 
-
-    def computeRPS_Lite(self): # 画像取得からパーセンテージの計算までを行います   maxtrialは撮影失敗時に取り直す回数、 amountは最終的に出力する枚数、durationは撮影間隔
+    #骨格推定と確率の評価を実行 (改良版) -------------------------------------------------------------------------------------------------
+    def computeRPS_Lite(self): # 画像取得からパーセンテージの計算までを行います
 
 
         img = self.getCameraImage()
-
+        #骨格推定を実行
         self.__tracker.doTracking(img,debug=True,useColor=False)
                 
         if self.__tracker.getNormalizedPosition().get(0) == None: #トラッキング成功時はそれを判定へ回す
             return False
 
-
+        #結果を結果判定に登録
         rate,gesture = self.__judge.finPos2rpsRate(self.__tracker)
         self.rsp_rates.append(rate)
         self.rsp_gestures.append(gesture)
@@ -122,7 +132,8 @@ class Battle:
             img[210:290 ,0:int(rpsRate['paper']*500)] = (255,0,0)
         cv2.imshow(name, img)
 
-    
+
+    #入力した結果に対して勝つ手を表示する -------------------------------------------------------------------------------------------------------------
     def showResultImg(self,ret):
         match ret:
             case "rock":
@@ -133,72 +144,80 @@ class Battle:
                 print("相手：チョキ")     
 
 
-    
+
+
+    #カウンタやフラグなど。1試合ごとにリセットされる    
     shotCounter = 0#撮影枚数カウンタ
     shotCounter_timeout = 0#撮影失敗枚数
     prev_shot_delta_time = 0#前回撮影時のcountを記録
+    oneshotFlags = [True] * 6   #タイムラインで1度だけ実行部分を作るためのフラグ
 
-    
-
-    def playOneMatch(self,timeCounter):# 1試合分の一連の処理を行います ----------------------------------------------------------------------------------------------------------------------------------
+    # 1試合分の一連の処理を行います ----------------------------------------------------------------------------------------------------------------------------------
+    def playOneMatch(self,timeCounter):#timeCounterには試合開始時を0とした秒数が入ります。
 
         duration = 10 #撮影間隔
-        amount = 7 #撮影枚数
-        timeout = 20
+        amount = 10 #撮影枚数
+        timeout = 20#撮影失敗時の再試行を許す最大時間
 
         ##カメラからの映像を常に表示
         cv2.imshow(self.battle_window,self.getCameraImage())
 
         if 0 < timeCounter <= 1:
-            if(self.oneshotFlags[0]):
+            if(self.oneshotFlags[0]):#1度だけ実行
                 print("最初は")
                 self.oneshotFlags[0] = False
         if 1 < timeCounter <= 2:
-            if(self.oneshotFlags[1]):
+            if(self.oneshotFlags[1]):#1度だけ実行
                 print("グー")
                 self.oneshotFlags[1] = False
 
         if 2 < timeCounter <= 2.5:
-            if(self.oneshotFlags[2]):
+            if(self.oneshotFlags[2]):#1度だけ実行
 
                 print("じゃん")
                 self.oneshotFlags[2] = False
         if 2.5 <= timeCounter <= 2.9:
-            if(self.oneshotFlags[3]):
+            if(self.oneshotFlags[3]):#1度だけ実行
                 print("けん")
                 self.oneshotFlags[3] = False
-        if 2.85 <= timeCounter:
-            start_time = 0
-            if self.shotCounter < amount:
-                start_time = time.time()
-                if self.prev_shot_delta_time == 0:
-                    if self.computeRPS_Lite():
-                        self.shotCounter += 1
 
-                        self.prev_shot_delta_time = counter
-                    elif counter - self.prev_shot_delta_time > timeout / 1000:
+        if 2.85 <= timeCounter:
+
+            start_time = 0#処理時間計測用
+
+            if self.shotCounter < amount: #撮影枚数以下なら撮影を試す
+                start_time = time.time()#処理時間計測用
+
+                if self.prev_shot_delta_time == 0: #1枚目を撮影
+                    if self.computeRPS_Lite():
+                        self.shotCounter += 1 #撮影成功時は撮影枚数カウンタを増やす
+
+                        self.prev_shot_delta_time = counter #次のフレームの撮影まで待機させたいのでカウンタでいろいろやる。
+
+                    elif counter - self.prev_shot_delta_time > timeout / 1000: #撮影失敗時は再試行するが、指定時間を超えたら撮影をあきらめる
                         self.shotCounter += 1
                         self.shotCounter_timeout += 1
 
 
-                elif counter - self.prev_shot_delta_time  > duration /1000 :
+                elif counter - self.prev_shot_delta_time  > duration /1000 : #撮影間隔を空けるための部分
                     self.prev_shot_delta_time = 0
 
 
             if self.shotCounter >= amount:#骨格推定と判定
-                if self.oneshotFlags[5]:
-                    end_time = time.time()
+                if self.oneshotFlags[5]:#1度だけ実行
+                    end_time = time.time()#処理時間計測
                     print(self.rsp_rates)
                     print(self.rsp_gestures)
                     print("処理時間" + str(end_time - start_time))
                     print("撮影成功枚数" + str(self.shotCounter - self.shotCounter_timeout))
+
                     #デバッグ用改造済みshowRpsRate
-                    #for i in range(len(self.rsp_rates)):
-                    #    self.showRpsRate(self.rsp_rates[i],str(i))
+                    for i in range(len(self.rsp_rates)):
+                        self.showRpsRate(self.rsp_rates[i],str(i))
                     
                     #print(self.__finalans.get_finalanswer(self.rsp_gestures,None))
 
-                    self.showResultImg(self.__finalans.get_finalanswer(self.rsp_gestures,None))
+                    self.showResultImg(self.__finalans.get_finalanswer(self.rsp_gestures,None))#最終結果を結果表示部分に渡す
 
                     self.oneshotFlags[5] = False
                     
@@ -211,11 +230,14 @@ class Battle:
             return 0
 
 
-    def battleFlow(self,timeCounter): #試合の一連を処理します ------------------------------------------------------------------------------------------------------------------------------------------
+    #試合の一連を処理します ------------------------------------------------------------------------------------------------------------------------------------------
+    def battleFlow(self,timeCounter): 
 
         if self.playOneMatch(timeCounter) == 0: #試合が終わったらそれを呼び出し元に通知
             return 0
-        
+        return 1
+
+    #フラグなどをリセットします
     def reset(self):
         self.oneshotFlags = [True] * len(self.oneshotFlags) 
         self.prev_shot_delta_time = 0
@@ -224,6 +246,7 @@ class Battle:
         self.rsp_rates = []
         self.rsp_gestures = []
 
+    #infinicamの終了処理
     def close(self):
         if self.__infinicam != None:
             self.__infinicam.close()
@@ -243,9 +266,7 @@ if __name__ == "__main__":
 
     deltaTime = 0.0
     counter = 0.0#経過時間のカウンター (試合開始時にリセット)
-     #一度だけ実行用のフラグ
 
-    
 
     while True:
         start_time = time.time()
@@ -253,18 +274,18 @@ if __name__ == "__main__":
 
         #試合を行う
         
-        ret = battle.battleFlow(counter)
-
+        ret = battle.battleFlow(counter) #retが0で試合終了を示す
         if ret == 0:#試合終了でカウンターをリセットし、再戦
             battle.reset()
             counter = 0
 
+        #カメラ切り替え
         if key == ord('q'):
-           battle.changeCamera(0)
+            battle.changeCamera(0)
         if key == ord('w'):
             battle.changeCamera(1)
 
-        if key == 27:
+        if key == 27:#ESCキーで終了
            break
 
         #タイマー
